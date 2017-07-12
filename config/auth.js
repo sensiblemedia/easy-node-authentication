232bf2f65ba669a46443fc7ada0e804270924f4f
Stephen Goldsmith <sg@Stephens-MacBook-Pro.local>
2017-07-12 21:35:50 +0100

    u

--- config/auth.js
+++ config/auth.js
@@ -12,7 +12,7 @@ module.exports = {
         'clientID'        : facebookAppId, // your App ID
         'clientSecret'    : facebookAppSecret, // your App Secret
         'callbackURL'     : 'http://auth-nodejs-authenticate.127.0.0.1.nip.io/auth/facebook/callback',
-        'profileURL': 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
+        'profileURL'      : 'https://graph.facebook.com/v2.5/me?fields=first_name,last_name,email'
 
     },
 
